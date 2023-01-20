c91ed94fc9b8f15e7dc4307c3b6e99fdbbc5792d
yuansui <yuansui08@gmail.com>
2023-01-20 22:17:38 +0800

    add zero-shot generation code

--- unified_babel_convertor.sh
+++ unified_babel_convertor.sh
@@ -19,6 +19,20 @@ python unified_babel_convertor.py --task totto tabfact hybridqa sqa feverous --o
 # 0_1_2
 python unified_babel_convertor.py --task totto tabfact hybridqa sqa feverous --objective oneshot --split validation --linear_func html --unified --unified_file_output ./exps/downstream_tasks_20230119_manual_log/0_1_2 --use_partition_mark --add_table_size --use_role_prompting
 
+
+# generate table-based downstream tasks (zero-shot)
+python unified_babel_convertor.py --task totto tabfact hybridqa sqa feverous --objective zeroshot --split validation --linear_func html --unified --unified_file_output ./exps/downstream_tasks_20230120_manual_zeroshot_log/0_1_2_3 --use_role_prompting --add_table_size --use_format_explanation --use_partition_mark
+# 0_2_3
+python unified_babel_convertor.py --task totto tabfact hybridqa sqa feverous --objective zeroshot --split validation --linear_func html --unified --unified_file_output ./exps/downstream_tasks_20230120_manual_zeroshot_log/0_2_3 --use_role_prompting --use_format_explanation --use_partition_mark
+# 0_1_3
+python unified_babel_convertor.py --task totto tabfact hybridqa sqa feverous --objective zeroshot --split validation --linear_func html --unified --unified_file_output ./exps/downstream_tasks_20230120_manual_zeroshot_log/0_1_3 --add_table_size --use_role_prompting --use_format_explanation
+# 1_2_3
+python unified_babel_convertor.py --task totto tabfact hybridqa sqa feverous --objective zeroshot --split validation --linear_func html --unified --unified_file_output ./exps/downstream_tasks_20230120_manual_zeroshot_log/1_2_3 --use_format_explanation --add_table_size --use_partition_mark
+# 0_1_2
+python unified_babel_convertor.py --task totto tabfact hybridqa sqa feverous --objective zeroshot --split validation --linear_func html --unified --unified_file_output ./exps/downstream_tasks_20230120_manual_zeroshot_log/0_1_2 --use_partition_mark --add_table_size --use_role_prompting
+
+
+
 # generate self-augmented information
 python unified_babel_convertor.py --task totto tabfact hybridqa sqa feverous --objective heur_8 heur_9 heur_10 --split validation --unified --unified_file_output ./exps/downstream_tasks_20230119_self_augmented_log/main --linear_func html --use_format_explanation --add_table_size --use_partition_mark
 python unified_babel_convertor.py --task tabfact feverous --objective heur_0 heur_1 heur_2 heur_3 heur_5 heur_7 --split validation --unified --unified_file_output ./exps/downstream_tasks_20230119_self_augmented_log/minor --linear_func html --use_format_explanation --add_table_size --use_partition_mark
