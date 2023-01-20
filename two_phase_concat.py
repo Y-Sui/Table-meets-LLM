37e33e8a55329ee47d1d6b77f32e55ec44acf8c0
yuansui <yuansui08@gmail.com>
2023-01-21 00:10:30 +0800

    modify concat_heur

--- two_phase_concat.py
+++ two_phase_concat.py
@@ -17,66 +17,85 @@ def read_json(path: str):
     with open(path, 'r', encoding='utf-8') as f:
         return json.load(f)
 
+def save_jsonl(path, content_list: list):
+    os.makedirs(path, exist_ok=True)
+    with open(os.path.join(path, "validation.jsonl"), "w") as outfile:
+        for content in content_list:
+            outfile.write(json.dumps(content) + "\n")
+
+def get_task_span(file_path):
+    task_file, row_index = [], []
+    with open(file_path, "r") as txt_file:
+        task_spans = txt_file.readlines() # ../generated/dart/heur_7\validation.jsonl, Row: [31076, 33844]
+    for i in range(len(task_spans)):
+        if args.job == "zero-shot":
+            if not task_spans[i].split(",")[0].__contains__("zero"):
+                continue
+        elif args.job == "linear":
+            if not task_spans[i].split(",")[0].__contains__("linear"):
+                continue
+        task_file.append(task_spans[i].split(",")[0])
+        row_index_number = task_spans[i].split(": ")[-1].replace("[", "").replace("]", "").replace("\n", "").split(",") # list
+        row_index.append({"start": int(row_index_number[0]), "end": int(row_index_number[1])})
+    return task_file, row_index
 
 def get_arguments():
     # Required parameters
     parser = argparse.ArgumentParser()
-    parser.add_argument("--src_dir", default="./exps/downstream_tasks_20230119_self_augmented_log/main/text003_760816f6-3bd1-4eae-917c-9e933973a6a8/output_dataset/validation_samples.0.jsonl",
-                        help="Directory of samples")
-    parser.add_argument("--split_dir", default="./exps/downstream_tasks_20230119_self_augmented_log/main/validation.json",
-                        help="Directory of split info")
-    parser.add_argument("--ref_dir", default="exps/downstream_tasks_20230119_manual_log/0_1_2_3/validation.jsonl",
-                        help="Directory of completion containing downstream groundtruth")
-    parser.add_argument("--to_dir", default="./exps/downstream_tasks_20230120_self_augmented_p2_log",
-                        help="Output directory")
+    parser.add_argument("--src_dir", default="./exps/downstream_tasks_20230120_self_augmented_p2_log", help="Directory of samples")
+    parser.add_argument("--to_dir", default="./exps/downstream_tasks_20230120_self_augmented_p2_revision_log", help="Output directory")
+    parser.add_argument("--heur", default="heur_8")
     return parser.parse_args()
-
-def save_unified_jsonl(output_path: str, unified_dict: dict):
-    os.makedirs(output_path, exist_ok=True)
-    content_list = []
-    split = 0
-    with open(f"{output_path}/validation.txt", "a", encoding="utf-8") as log_f:
-        for index, value in enumerate(unified_dict["content"]):
-            info = unified_dict["task"][index] + "|" + unified_dict["objective"][index]
-            start = split
-            end = split + len(value)
-            span_log = [start, end]
-            log_f.write(f"{info}, Row: {span_log}\n")
-            split = end
-            content_list.append(value)
-    with open(f"{output_path}/validation.jsonl", "w") as outfile:
-        for content in content_list:
-            for ele in content:
-                outfile.write(json.dumps(ele) + "\n")
 
 if __name__ == '__main__':
     args = get_arguments()
 
-    one_shot_split_info = read_json("./exps/downstream_tasks_20230119_self_augmented_log/main/1-shot.json")
-    split_info = read_json(args.split_dir)
-    sample_results = read_jsonl(args.src_dir)
-    references = read_jsonl(args.ref_dir)
+    src_dir = args.src_dir
+    heur_idx = args.heur
+    references = read_jsonl(os.path.join(src_dir, heur_idx, "validation.jsonl"))
+    task_file, row_index = get_task_span(os.path.join(src_dir, heur_idx, "validation.txt"))
+    model_path = os.listdir(os.path.join(src_dir, heur_idx))
+    for i in model_path:
+        if i.__contains__("text003"):
+            info_path = i
+    new_content_info = []
+    additional_info = read_jsonl(os.path.join(src_dir, heur_idx, info_path, "output_dataset", "validation_samples.0.jsonl"))
+    for task_idx in range(len(task_file)):
+        task_name = task_file[task_idx]
+        start_idx, end_idx = row_index[task_idx]["start"], row_index[task_idx]["end"]
+        ref = references[start_idx: end_idx]
+        info = additional_info[start_idx: end_idx]
+        for i in range(len(ref)):
+            example = ref[i]["example"]
+            prompt = ref[i]["prompt"]
+            completion = ref[i]["completion"]
+            add_info = info[i]['samples'][0]
+            prefix, suffix = prompt.split("<request>")
+            new_prompt = example + prefix + "<information>\n" + add_info + "\n<request>\n" + get_requests(task_name) + suffix
+            new_content_info.append({"prompt": new_prompt, "completion": completion})
 
-    unified_dict = {"content": [], "task": [], "objective": []}
+    save_jsonl(os.path.join(src_dir, heur_idx), new_content_info)
 
-    # loop for different downstream tasks: totto ...
-    for task, objs in split_info.items():
-        # loop for different objectives: heur_1 ...
-        for obj, line_span in objs.items():
-            start_idx, end_idx = line_span
-            task_results = sample_results[start_idx: end_idx]
-            ref = references[one_shot_split_info[task][0]: one_shot_split_info[task][1]]
-            new_split_info = []
-            for i in range(len(task_results)):
-                prompt = ref[i]["prompt"]
-                sample = task_results[i]['samples'][0]  # only has 1 sample
-                prefix, suffix = prompt.split('<request>')
-                new_prompt = prefix + "<information>\n" + sample + "\n<request>\n" + get_requests(task) + suffix
-                completion = ref[i]["completion"]
-                new_split_info.append({"prompt": new_prompt, "completion": completion})
-            unified_dict["content"].append(new_split_info)
-            unified_dict["task"].append(task)
-            unified_dict["objective"].append(obj)
+    # unified_dict = {"content": [], "task": [], "objective": []}
 
-    save_unified_jsonl(args.to_dir, unified_dict)
+    # # loop for different downstream tasks: totto ...
+    # for task, objs in split_info.items():
+    #     # loop for different objectives: heur_1 ...
+    #     for obj, line_span in objs.items():
+    #         start_idx, end_idx = line_span
+    #         task_results = sample_results[start_idx: end_idx]
+    #         ref = references[one_shot_split_info[task][0]: one_shot_split_info[task][1]]
+    #         new_split_info = []
+    #         for i in range(len(task_results)):
+    #             prompt = ref[i]["prompt"]
+    #             sample = task_results[i]['samples'][0]  # only has 1 sample
+    #             prefix, suffix = prompt.split('<request>')
+    #             new_prompt = prefix + "<information>\n" + sample + "\n<request>\n" + get_requests(task) + suffix
+    #             completion = ref[i]["completion"]
+    #             new_split_info.append({"prompt": new_prompt, "completion": completion})
+    #         unified_dict["content"].append(new_split_info)
+    #         unified_dict["task"].append(task)
+    #         unified_dict["objective"].append(obj)
+    #
+    # save_unified_jsonl(args.to_dir, unified_dict)
 
