7854e598199db9b3abb3e5623880ad0101d3cdb5
v-yuansui <v-yuansui@microsoft.com>
2023-06-06 12:54:37 +0800

    Modify the framework

--- unified_benchmark_generate.sh
+++ unified_benchmark_generate.sh
@@ -1,16 +1,16 @@
 ## zero-shot
-#python unified_benchmark_generator.py --dataset tabfact feverous sqa hybridqa totto --objective zero-shot --split validation --linearize_list html --use_partition_mark --use_format_explanation --use_role_prompting
+python unified_benchmark_generator.py --dataset tabfact feverous sqa hybridqa totto --objective zero-shot --split validation --linearize_list html --use_partition_mark --use_format_explanation --use_role_prompting
 # all format without 1-shot
 python unified_benchmark_generator.py --dataset tabfact feverous sqa hybridqa totto --objective zero-shot --split validation --use_partition_mark --use_format_explanation --use_role_prompting --linearize_list html markdown xml json nl_sep
 #=============================
 ## few-shot
-#python unified_benchmark_generator.py --dataset tabfact feverous sqa hybridqa totto --objective few-shot --split validation --linearize_list html --use_partition_mark --use_format_explanation --use_role_prompting
+python unified_benchmark_generator.py --dataset tabfact feverous sqa hybridqa totto --objective few-shot --split validation --linearize_list html --use_partition_mark --use_format_explanation --use_role_prompting
 ##=============================
 ## 1-shot, 0_1_3
-#python unified_benchmark_generator.py --dataset tabfact feverous sqa hybridqa totto --objective 1-shot --split validation --use_format_explanation --use_partition_mark --use_role_prompting --linearize_list html markdown xml json nl_sep
+python unified_benchmark_generator.py --dataset tabfact feverous sqa hybridqa totto --objective 1-shot --split validation --use_format_explanation --use_partition_mark --use_role_prompting --linearize_list html markdown xml json nl_sep
 ## w/o role_prompting 0_1
-#python unified_benchmark_generator.py --dataset tabfact feverous sqa hybridqa totto --objective 1-shot --split validation --use_format_explanation --use_partition_mark --linearize_list html markdown xml json nl_sep
+python unified_benchmark_generator.py --dataset tabfact feverous sqa hybridqa totto --objective 1-shot --split validation --use_format_explanation --use_partition_mark --linearize_list html markdown xml json nl_sep
 ## w/o partition_mark 1_3
-#python unified_benchmark_generator.py --dataset tabfact feverous sqa hybridqa totto --objective 1-shot --split validation --use_format_explanation --use_role_prompting --linearize_list html markdown xml json nl_sep
+python unified_benchmark_generator.py --dataset tabfact feverous sqa hybridqa totto --objective 1-shot --split validation --use_format_explanation --use_role_prompting --linearize_list html markdown xml json nl_sep
 ## w/o format_explanation 0_3
-#python unified_benchmark_generator.py --dataset tabfact feverous sqa hybridqa totto --objective 1-shot --split validation --use_role_prompting --use_partition_mark --linearize_list html markdown xml json nl_sep
+python unified_benchmark_generator.py --dataset tabfact feverous sqa hybridqa totto --objective 1-shot --split validation --use_role_prompting --use_partition_mark --linearize_list html markdown xml json nl_sep
--- utils/evaluate_tools.py
+++ utils/evaluate_tools.py
@@ -3,11 +3,11 @@ import os
 import csv
 import pandas as pd
 
+
 # load the evaluation results from json file
 def load_results_from_json(dir_path: str, log_dir_name: str):
-    
     dir_path_files = []
-    
+
     # search the dir_path if there is a log_dir_name
     for file in os.listdir(dir_path):
         if file.__contains__(log_dir_name):
@@ -19,40 +19,88 @@ def load_results_from_json(dir_path: str, log_dir_name: str):
 
     return dir_path_files
 
+
 def convert_json_to_csv(json_data: list, output_file_name: str):
-    
     # save the results to csv file
-    with open(output_file_name, "w", newline=['Choice', 'TabFact', 'HybridQA', 'SQA', 'Feverous', 'ToTTo', 'ToTTo', 'ToTTo', 'ToTTo']) as csv_file:
+    with open(
+        output_file_name,
+        "w",
+        newline=[
+            'Choice',
+            'TabFact',
+            'HybridQA',
+            'SQA',
+            'Feverous',
+            'ToTTo',
+            'ToTTo',
+            'ToTTo',
+            'ToTTo',
+        ],
+    ) as csv_file:
         writer = csv.writer(csv_file, delimiter=',')
         for i in range(len(json_data)):
             for key, value in json_data[i].items():
                 task, file_format, metric = key.split("_")
                 writer.writerow([task, file_format, metric, value])
-                
-def convert_pandas_to_csv(json_data: list, output_file_name:str):
-    
-    df = pd.DataFrame(columns=['Choice', 'TabFact', 'HybridQA', 'SQA', 'Feverous', 'ToTTo', 'ToTTo', 'ToTTo', 'ToTTo'],
-                      index=["NL+Sep", "NL+Sep w/o format explanation", "NL+Sep w/o partition mark", "NL+Sep w/o role prompting",
-                             "Markdown", "Markdown w/o format explanation", "Markdown w/o partition mark", "Markdown w/o role prompting",
-                             "Json", "Json w/o format explanation", "Json w/o partition mark", "Json w/o role prompting",
-                             "XML", "XML w/o format explanation", "XML w/o partition mark", "XML w/o role prompting",
-                             "HTML", "HTML w/o format explanation", "HTML w/o partition mark", "HTML w/o role prompting"])
-    
+
+
+def convert_pandas_to_csv(json_data: list, output_file_name: str):
+    df = pd.DataFrame(
+        columns=[
+            'Choice',
+            'TabFact',
+            'HybridQA',
+            'SQA',
+            'Feverous',
+            'ToTTo',
+            'ToTTo',
+            'ToTTo',
+            'ToTTo',
+        ],
+        index=[
+            "NL+Sep",
+            "NL+Sep w/o format explanation",
+            "NL+Sep w/o partition mark",
+            "NL+Sep w/o role prompting",
+            "Markdown",
+            "Markdown w/o format explanation",
+            "Markdown w/o partition mark",
+            "Markdown w/o role prompting",
+            "Json",
+            "Json w/o format explanation",
+            "Json w/o partition mark",
+            "Json w/o role prompting",
+            "XML",
+            "XML w/o format explanation",
+            "XML w/o partition mark",
+            "XML w/o role prompting",
+            "HTML",
+            "HTML w/o format explanation",
+            "HTML w/o partition mark",
+            "HTML w/o role prompting",
+        ],
+    )
+
     for i in range(len(json_data)):
         for key, value in json_data[i].items():
             _ = key.replace("['", "").replace("']", "").split("_")
-            task, input_choices, metric = _[0].title(), format_mapping_func('_'.join(_[1:-1])), _[-1]
+            task, input_choices, metric = (
+                _[0].title(),
+                format_mapping_func('_'.join(_[1:-1])),
+                _[-1],
+            )
             df.loc[input_choices, task] = value
-            
+
     print(df)
-    
+
+
 def format_mapping_func(input_choices: str):
     _ = input_choices.split("_")
-    
+
     format, option = _[0], "_".join(_[1:])
     if option.__contains__("sep"):
         option.replace("sep_", "")
-        
+
     if format == "nl":
         if option == "0_1":
             return "NL+Sep w/o role prompting"
@@ -62,7 +110,7 @@ def format_mapping_func(input_choices: str):
             return "NL+Sep w/o format explanation"
         elif option == "1_3":
             return "NL+Sep w/o partition mark"
-        
+
     elif format == "markdown":
         if option == "0_1":
             return "Markdown w/o role prompting"
@@ -72,7 +120,7 @@ def format_mapping_func(input_choices: str):
             return "Markdown w/o format explanation"
         elif option == "1_3":
             return "Markdown w/o partition mark"
-        
+
     elif format == "xml":
         if option == "0_1":
             return "xml w/o role prompting"
@@ -82,7 +130,7 @@ def format_mapping_func(input_choices: str):
             return "xml w/o format explanation"
         elif option == "1_3":
             return "xml w/o partition mark"
-        
+
     elif format == "markdown":
         if option == "0_1":
             return "markdown w/o role prompting"
@@ -92,7 +140,7 @@ def format_mapping_func(input_choices: str):
             return "markdown w/o format explanation"
         elif option == "1_3":
             return "markdown w/o partition mark"
-    
+
     elif format == "json":
         if option == "0_1":
             return "json w/o role prompting"
@@ -102,7 +150,7 @@ def format_mapping_func(input_choices: str):
             return "json w/o format explanation"
         elif option == "1_3":
             return "json w/o partition mark"
-        
+
     elif format == "html":
         if option == "0_1":
             return "html w/o role prompting"
@@ -112,10 +160,9 @@ def format_mapping_func(input_choices: str):
             return "html w/o format explanation"
         elif option == "1_3":
             return "html w/o partition mark"
-            
-    
+
+
 dir_path = "exps"
 log_dir_name = "downstream_tasks_20230308_self_augmented_p2_logheur_8"
-    
+
 convert_pandas_to_csv(load_results_from_json(dir_path, log_dir_name), "")
-            
